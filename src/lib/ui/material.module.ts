65c518802d5b04cd6232e2d2f18bbc09f7662f26
Jim <jim.kodingwork@gmail.com>
2022-05-09 21:09:43 +0800

    feat: add tool bar

--- src/lib/ui/material.module.ts
+++ src/lib/ui/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatCardModule, MatGridListModule, MatTableModule],
-  exports: [MatCardModule, MatGridListModule, MatTableModule],
+  imports: [MatCardModule, MatGridListModule, MatTableModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [MatCardModule, MatGridListModule, MatTableModule, MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class MaterialModule {}
