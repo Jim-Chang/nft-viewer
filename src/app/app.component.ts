c59c5fc6e6489b47e47e22e5f483a04080e03278
Jim <jim.kodingwork@gmail.com>
2022-05-14 22:16:21 +0800

    feat: add chain name to nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { chainIdToName, Web3ProviderService } from 'Lib/services/web3-provider.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'nft-viewer';
+  chainName: string;
+
+  constructor(private web3Service: Web3ProviderService) {
+    this.web3Service.getChainName$().subscribe((name) => (this.chainName = name));
+    this.web3Service.chainChanged$().subscribe((chainId) => {
+      this.chainName = chainIdToName(chainId);
+    });
+  }
 }
