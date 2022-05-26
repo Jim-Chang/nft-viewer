21bbd8638204f5094ec74333ad85746ca5875991
Jim <jim.kodingwork@gmail.com>
2022-05-26 11:56:48 +0800

    feat: initial publisher app

---
+++ projects/kong-long-nft-publisher/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
