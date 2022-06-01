192fbca85a8f52ce57a0b4f180676b0eef75acd0
Jim <jim.kodingwork@gmail.com>
2022-06-01 13:25:32 +0800

    feat: add estimate gas and get gas price before send mint transaction

--- projects/kong-long-nft/src/app/contract/kong-long-nft.ts
+++ projects/kong-long-nft/src/app/contract/kong-long-nft.ts
@@ -1,8 +1,8 @@
 import { TTransaction } from './../../../../lib-web3/src/lib/services/web3-provider.type';
 import ABI_KONG_LONG_NFT from './KongLongNFT.json';
 import { ERC721 } from 'projects/lib-web3/src/lib/contracts/erc-721';
-import { from, Observable } from 'rxjs';
-import { shareReplay, switchMap } from 'rxjs/operators';
+import { from, Observable, forkJoin } from 'rxjs';
+import { shareReplay, switchMap, mergeMap } from 'rxjs/operators';
 import { AbiItem } from 'web3-utils';
 
 export class KongLongNFT extends ERC721 {
@@ -22,8 +22,16 @@ export class KongLongNFT extends ERC721 {
   }
 
   mint(owner: string, tokenURI: string, fromAccount: string): Observable<TTransaction> {
-    return from(
-      this.web3Contract.methods.mintToken(owner, tokenURI).send({ from: fromAccount }),
+    const mintToken = this.web3Contract.methods.mintToken(owner, tokenURI);
+    const getGasPrice$ = from(this.web3.eth.getGasPrice());
+    const estimateGas$ = from(mintToken.estimateGas({ from: fromAccount }));
+
+    return forkJoin([getGasPrice$, estimateGas$]).pipe(
+      mergeMap(([gasPrice, estimateGas]) => {
+        const gas = Math.ceil((estimateGas as number) * 1.5);
+        console.log(`gas price: ${gasPrice}, estimate gas: ${estimateGas}, max gas: ${gas}`);
+        return from(mintToken.send({ from: fromAccount, gasPrice, gas }));
+      }),
     ) as Observable<TTransaction>;
   }
 
